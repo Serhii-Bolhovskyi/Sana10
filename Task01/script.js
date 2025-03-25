6cc58f4570244e491bb7a24cf96b5391fc4ef5bb
Serhii-Bolhovskyi <azeror72@gmail.com>
2025-03-25 22:12:45 +0100

    do first task Task01, add /.idea in gitignore

---
+++ Task01/script.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('body');
+body.style.fontFamily = "Arial";
+
+const div1 = document.getElementById("div1");
+div1.style.cssText =
+    "color: orange; background: yellow; fontSize: 16px; padding: 5px; text-align: center;";
+
+const div2 = document.getElementById("div2");
+div2.style.cssText =
+    "color: blue; background: skyblue; fontSize: 14px; padding: 5px; text-align: right;";
+const div3 = document.getElementById("div3");
+div3.style.cssText =
+    "color: red; background: lightcoral; fontSize: 12px; padding: 5px; text-align: left;";
